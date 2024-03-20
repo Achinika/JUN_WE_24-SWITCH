70d7267f8b8fd550923044e4a46beecd418cee9e
It21055508 <87391005+Osanda27@users.noreply.github.com>
2024-03-20 19:13:26 +0530

    undo changes

--- backend/Routes/businessRoute.js
+++ backend/Routes/businessRoute.js
@@ -1,9 +1,1 @@
-import express from "express";
-import { getbusiness } from "../Controllers/businessController.js";
-
-const router = express.Router();
-
-router.get('/',getbusiness)
-
-export default router;
 
--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import mongoose  from 'mongoose';
 import dotenv from 'dotenv';
 import AuthRoute from './Routes/AuthRoute.js'
-import businessRoute from './Routes/businessRoute.js'
+
 
 //Routes
 
@@ -30,4 +30,3 @@ mongoose.connect(process.env.MONGO_DB)
 
 //usage of routes
 app.use('/auth',AuthRoute)
-app.use('/business',businessRoute)
