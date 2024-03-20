82c04aeb984f32eff0616952d0138a9962f55448
Kavindi Arangala <IT21208362@my.sliit.lk>
2024-03-20 16:18:05 +0530

    index.js-update changes with employers
    update users to employers

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,11 @@ import mongoose  from 'mongoose';
 import dotenv from 'dotenv';
 import AuthRoute from './Routes/AuthRoute.js'
 
-//Routes
-
-
-
-
-
+import UserModel from './Models/userModel.js';
 
 //Middleware-create instant for the server
 const app = express();
+
 //enable middleware
 app.use(bodyParser.json({limit:'30mb', extended:true})); //parse incoming requests of
 app.use(bodyParser.urlencoded({limit:'30mb', extended:true}));
@@ -28,4 +24,42 @@ mongoose.connect(process.env.MONGO_DB)
 
 
 //usage of routes
-app.use('/auth',AuthRoute)
+app.use('/auth',AuthRoute);
+
+
+
+// Define the route for switching to employer account
+app.get('/auth/switchEmployer/:userId', async(req, res) => {
+    const userId = req.params.userId;
+
+    try {
+        //find user by id
+        const user = await UserModel.findById(userId).select('isEmployer');
+        
+        if(!user){
+            return  res.status(404).json ({message: 'User Not Found'});
+        }
+
+        //update user account to employer
+        user.accountType = 'employer';
+
+        //save update user data
+        await user.save();
+        res.status(200).json({ message: 'User account switched to employer' });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+// Handle undefined routes
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!");
+});
+
+// Handle errors
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
