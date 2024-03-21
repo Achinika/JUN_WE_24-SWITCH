20a8ee281f737d3f7a4f7ffe82c4a43c8142e8c7
It21055508 <87391005+Osanda27@users.noreply.github.com>
2024-03-22 00:16:37 +0530

    Define the route for switching to Business account

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,8 @@ app.use('/auth',AuthRoute);
 
 
 
-// Define the route for switching to employer account
+// ///// Define the route for switching to employer account ///////////////////////////////////////////////////////////////
+
 app.get('/auth/switchEmployer/:userId', async(req, res) => {
     const userId = req.params.userId;
 
@@ -52,6 +53,35 @@ app.get('/auth/switchEmployer/:userId', async(req, res) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 });
+
+
+
+/////// Define the route for switching to Business account ///////////////////////////////////////////////////////////////
+
+app.get('/auth/switchBusiness/:userId', async(req, res) => {
+    const userId = req.params.userId;
+
+    try {
+        //find user by id
+        const user = await UserModel.findById(userId).select('isBusiness');
+        
+        if(!user){
+            return  res.status(404).json ({message: 'User Not Found'});
+        }
+
+        //update user account to employer
+        user.accountType = 'Business';
+
+        //save update user data
+        await user.save();
+        res.status(200).json({ message: 'User account switched to Business' });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
 
 // Handle undefined routes
 app.use((req, res, next) => {
