a5704ac570de9daf193e6c4c4a9c6126426b79f4
Kavindi Arangala <IT21208362@my.sliit.lk>
2024-03-22 19:38:35 +0530

    Update allUsersController.js

--- backend/Controllers/allUsersController.js
+++ backend/Controllers/allUsersController.js
@@ -1,7 +1,3 @@
-import generalUserModel from "../Models/generalUserModel.js";
-import EmployerModel from "../Models/employerModel.js";
-import consultantModel from "../Models/consultantModel.js";
-import businessModel from "../Models/businessModel.js";
 import userModel from "../Models/userModel.js";
-
 import jwt from 'jsonwebtoken';
+
