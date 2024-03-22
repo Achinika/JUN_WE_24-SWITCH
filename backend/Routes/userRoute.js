dbe2f5f7aed01e9cec2f7cd0e3b0686a119cab20
ayunisemandi <99675915+IT21210792@users.noreply.github.com>
2024-03-22 18:01:14 +0530

    update auth route

--- backend/Routes/userRoute.js
+++ backend/Routes/userRoute.js
@@ -1,17 +1,23 @@
 import express from "express";
 import {getEmployer, updateEmpDetails} from '../Controllers/empController.js';
 import {getconsultant } from '../Controllers/cosultantController.js';
+import {getGenUser, updateGenDetails} from '../Controllers/generalUserController.js';
+
+import {getEnroll, updateEnrollDetails,postEnroll} from '../Controllers/userEnrollController.js';
+
+
+
 
 const  router = express.Router();
 
 //FOR GENERAL USERS
+router.get('/:userId', getGenUser); //get a Employer
+router.patch('/update/:userId', updateGenDetails); //update epmloyer
 
-
-
-
-
-
-
+// For Enroll User
+router.get('/:userId', getEnroll); //get a Employer
+router.patch('/update/:userId', updateEnrollDetails); //update epmloyer
+router.post('/add/:userId', postEnroll); //update epmloyer
 
 
 
@@ -35,6 +41,7 @@ router.patch('/update/:userId', updateEmpDetails); //update epmloyer
 
 
 //FOR BUSINESS 
+
 
 
 
