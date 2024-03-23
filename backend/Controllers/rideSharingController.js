3095b62bd35b006481692e653546ad9d7d73efe9
ayunisemandi <99675915+IT21210792@users.noreply.github.com>
2024-03-23 13:52:20 +0530

    update rideSharingController

--- backend/Controllers/rideSharingController.js
+++ backend/Controllers/rideSharingController.js
@@ -154,6 +154,37 @@ export const updateRidesharingDetails = async (req, res) => {
     }
 };
 
+//delete Ridesharing account from both db
+export const deleteRideAccount =async(req,res) => {
+    const userId = req.params.userId;
 
+    try {
+        // Extract the token from the request headers
+        const token = req.headers.authorization.split(' ')[1];
+
+        // Verify the token to obtain user data, including the user ID
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+        // Extract the user ID from the decoded token
+        const loggedInUserId = decodedToken.userId;
+
+        // Check if the user ID in the request matches the logged-in user ID
+        if (userId !== loggedInUserId) {
+            return res.status(403).json({ message: "You are not authorized to Delete this Account" });
+        }
+
+        // Find and delete the Ridesharing document
+        await RideModel.findOneAndDelete({ user: userId });
+
+        // Find and delete the user document
+        await UserModel.findByIdAndDelete(userId);
+
+        // Return success response
+        res.status(200).json({ message: "GenUser account deleted successfully" });
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
    
 
